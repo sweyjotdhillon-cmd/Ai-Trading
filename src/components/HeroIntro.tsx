7c63447ab37133133e080d3a7dcb9e9a3a71f1ba
sweyjotdhillon-cmd <sweyjotdhillon@gmail.com>
2026-05-14 05:39:08 +0530

    Tighten hero spacing above ticker on mobile

--- src/components/HeroIntro.tsx
+++ src/components/HeroIntro.tsx
@@ -83,7 +83,7 @@ export function HeroIntro({ onLaunch }: { onLaunch?: () => void }) {
       </main>
 
       {/* Embedded Terminal Preview */}
-      <div className="w-full relative z-10 px-4 sm:px-6 pb-12 overflow-hidden flex flex-col items-center">
+      <div className="w-full relative z-10 px-4 sm:px-6 pb-2 md:pb-4 overflow-hidden flex flex-col items-center">
         <TerminalPreview />
       </div>
 
